75c723235154e539ec726dd98d68566589c3c340
can <769658249@qq.com>
2023-03-13 09:28:19 +0800

    rocketMQ

---
+++ rocketmq/src/test/java/com/inesa/rocketmq/RocketmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inesa.rocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
